770628cadee2988b9701c9ce5e5363f9dbde3762
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2024-10-18 13:35:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,46 @@ data.feature_names
 
 data.target_names
 
+
+
+#----------EXAMPLE-------
+from sklearn.model_selection import KFold
+kf= KFold(n_splits=5,shuffle=True,random_state=42)
+
+#-----MODEL SELECTION
+from sklearn.neighbors import KNeighborsClassifier
+model=KNeighborsClassifier()
+my_params= { 'n_neighbors':[3,5,7,8,9],
+            'metric':['minkowski'  , 'euclidean' , 'manhattan'],
+           'weights':['uniform','distance']}
+
+from sklearn.model_selection import GridSearchCV
+
+gs=GridSearchCV(model,my_params,cv=kf,scoring='accuracy')
+gs.fit(x,y)
+gs.best_score_
+gs.best_params_
+
+
+
+#---------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 from sklearn.metrics import accuracy_score
 train_score=accuracy_score(y_true, y_pred)
 print(train_score) 
@@ -31,17 +71,17 @@ a=model.coef_
 b=model.intercept_
 print(a) 
 print(b) 
-
+'''
 #KNN 
 #step0 ---- cleaning
 #step1 ----
 import numpy as np
-x=np.array(data('load_breast_cancer'))
-y=np.array(data('load_breast_cancer'))
+#x=np.array(data('load_breast_cancer'))
+#y=np.array(data('load_breast_cancer'))
 
 #step2
-from sklearn.model_selection import load_breast_cancer
-x_train,x_test,y_train,y_test=train_test_split(x,y,size=0/25,shuffle=true,random_state=42)
+#from sklearn.model_selection import load_breast_cancer
+#x_train,x_test,y_train,y_test=train_test_split(x,y,size=0/25,shuffle=true,random_state=42)
 
 #step3
 from sklearn.nighbors import KNeighborsClassification
@@ -52,7 +92,8 @@ hyperparametr(faraparametr)
 model=KNeighborsClassification(n_neigbors=1) 
 
 #step4 ---- fitting
-model.fit(x_train,y_train)
+#model.fit(x_train,y_train)
+model.fit(x,y)
 
 #step5 ---- validating
 y_pred=model.predict(x_train) 
